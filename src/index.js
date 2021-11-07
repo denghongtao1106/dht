d3c33c342c1b6bb3c9cbea6263573ec242201f08
denghongtao1106 <dht_1106@qq.com>
2021-11-07 10:39:57 +0800

    提交

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*** src/index.js  ***/
+import React from 'react';
+import './style.css';
+import DhtComp from 'dht-react-comp';
+const MyComponent = () => (
+    <div>
+        dieie
+        <DhtComp />
+    </div>
+);
+export default MyComponent;
